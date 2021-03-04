0b7d36c72933beeedf22f7dc7a64f55d04be89af
Nixon <voxell.technologies@gmail.com>
2021-03-04 13:39:56 +0800

    update

--- Runtime/Shortcuts.cs
+++ Runtime/Shortcuts.cs
@@ -6,9 +6,9 @@ static class Shortcuts
   [MenuItem ("Tools/Clear Console %#d")] // CMD + SHIFT + D
   public static void ClearConsole()
   {
-    var assembly = Assembly.GetAssembly(typeof(SceneView));
-    var type = assembly.GetType("UnityEditor.LogEntries");
-    var method = type.GetMethod("Clear");
+    Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+    System.Type type = assembly.GetType("UnityEditor.LogEntries");
+    MethodInfo method = type.GetMethod("Clear");
     method.Invoke(new object(), null);
   }
 }

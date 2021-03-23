e8ba7e499f14289d6fc8026d62d64a2d9a2d109e
Nixon <voxell.technologies@gmail.com>
2021-03-23 18:45:43 +0800

    logging and communication protocol in place

---
+++ Runtime/Common/Logging.cs
@@ -0,0 +1,61 @@
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+The Original Code is Copyright (C) 2020 Voxell Technologies.
+All rights reserved.
+*/
+
+using UnityEngine;
+
+namespace SmartAssistant
+{
+  public enum LogStyle
+  {
+    Log,
+    Warning,
+    Error
+  }
+
+  public enum LogImportance
+  {
+    Normal,
+    Important,
+    Crucial,
+    Critical
+  }
+
+  public class Logging
+  {
+    public LogImportance debugLevel;
+
+    public Logging(LogImportance debugLevel)
+    {
+      this.debugLevel = debugLevel;
+    }
+
+    public void CustomLog(object message, LogImportance importance, LogStyle logStyle)
+    {
+      if (importance >= debugLevel)
+      {
+        if (logStyle == LogStyle.Log)
+          Debug.Log(message);
+        if (logStyle == LogStyle.Warning)
+          Debug.LogWarning(message);
+        if (logStyle == LogStyle.Error)
+          Debug.LogError(message);
+      }
+    }
+  }
+}
--- Runtime/Socket.cs
+++ Runtime/Socket.cs
@@ -17,111 +17,198 @@ The Original Code is Copyright (C) 2020 Voxell Technologies.
 All rights reserved.
 */
 
+using UnityEngine;
 using System;
-using System.Net; 
-using System.Net.Sockets; 
-using System.Text; 
-using System.Threading; 
-using UnityEngine;  
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
-public class Socket : MonoBehaviour
+using Newtonsoft.Json;
+
+namespace SmartAssistant.Core
 {
-  #region Socket Settings
-  [Header("Socket Settings")]
-  public static string ipAddress = "localhost";
-  public static int port = 8052;
-  #endregion
-
-  #region TCP Connections
-  // Background thread for TcpServer workload. 
-  public static Thread tcpListenerThread;
-  // TCPListener to listen for incomming TCP connection requests.
-  public static TcpListener tcpListener;
-  // Create handle to connected tcp client.
-  public static TcpClient connectedTcpClient;
-  #endregion
-
-  void Awake ()
+  public class Socket : MonoBehaviour
   {
-    // Start TcpServer background thread
-    tcpListenerThread = new Thread (new ThreadStart(StartListening));
-    tcpListenerThread.IsBackground = true;
-    tcpListenerThread.Start();
-  }
+    #region Socket Settings
+    [Header("Socket Settings")]
+    public string ipAddress = "localhost";
+    public int port = 8052;
+    public LogImportance debugLevel;
+    private Logging logging;
+    #endregion
 
-  void OnDisable()
-  {
-    tcpListenerThread.Abort();
-  }
+    #region TCP Connections
+    // Background thread for TcpServer workload. 
+    public static Thread tcpListenerThread;
+    // TCPListener to listen for incomming TCP connection requests.
+    public static TcpListener tcpListener;
+    // Create handle to connected tcp client.
+    public static TcpClient connectedTcpClient;
+    #endregion
 
-  void Update ()
-  {
-    if (Input.GetKeyDown(KeyCode.Space)) SendMessage();
-  }
-  
-  /// <summary> 
-  /// Runs in background TcpServerThread; Handles incomming TcpClient requests 
-  /// </summary> 
-  public static void StartListening()
-  {
-    try
+    #region Socket Events Handler
+    public static readonly int maxTaskType = 3;
+    public static Action[] socketActions;
+    public static Action<string>[] socketInputActions;
+    public static Func<string, string>[] socketFuncs;
+    #endregion
+
+    void Awake()
     {
-      // Create listener on localhost port 8052. 
-      tcpListener = new TcpListener(IPAddress.Parse(ipAddress), 8052); 
-      tcpListener.Start();              
-      Debug.Log("Server is listening");              
-      Byte[] bytes = new Byte[1024];  
-      while (true)
+      // Start TcpServer background thread
+      tcpListenerThread = new Thread (new ThreadStart(SocketListen));
+      tcpListenerThread.IsBackground = true;
+      tcpListenerThread.Start();
+
+      logging = new Logging(debugLevel);
+
+      print(socketActions.Length);
+    }
+
+    void OnDisable()
+    {
+      tcpListenerThread.Abort();
+    }
+
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space)) SocketSend("Testing");
+    }
+    
+    /// <summary> 
+    /// Runs in background TcpServerThread; Handles incomming TcpClient requests 
+    /// </summary> 
+    public void SocketListen()
+    {
+      try
       {
-        using (connectedTcpClient = tcpListener.AcceptTcpClient())
+        // Create listener on localhost port 8052. 
+        tcpListener = new TcpListener(IPAddress.Parse(ipAddress), 8052);
+        tcpListener.Start();
+        logging.CustomLog("Server is listening", LogImportance.Important, LogStyle.Log);
+        Byte[] bytes = new Byte[1024];
+        while (true)
         {
-          // Get a stream object for reading 
-          using (NetworkStream stream = connectedTcpClient.GetStream())
+          using (connectedTcpClient = tcpListener.AcceptTcpClient())
           {
-            int length;
-            // Read incomming stream into byte arrary.
-            while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+            // Get a stream object for reading 
+            using (NetworkStream stream = connectedTcpClient.GetStream())
             {
-              var incommingData = new byte[length];
-              Array.Copy(bytes, 0, incommingData, 0, length);
-              // Convert byte array to string message.
-              string clientMessage = Encoding.ASCII.GetString(incommingData);
-              Debug.Log("client message received as: " + clientMessage);
+              int length;
+              // Read incomming stream into byte arrary.
+              while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+              {
+                byte[] incommingData = new byte[length];
+                Array.Copy(bytes, 0, incommingData, 0, length);
+                string clientSocketMessage = Encoding.UTF8.GetString(incommingData);
+                SocketMessage socketMessage = JsonConvert.DeserializeObject<SocketMessage>(clientSocketMessage);
+                logging.CustomLog(
+                  $"task received from client\nTask: {socketMessage.task}, Argument: {socketMessage.argument}",
+                  LogImportance.Normal, LogStyle.Log
+                );
+
+                string methodOutput;
+                ExecuteSocketTask(socketMessage, out methodOutput);
+              }
             }
           }
         }
       }
-    }
-    catch (SocketException socketException)
-    {
-      Debug.Log("SocketException " + socketException.ToString());
-    }
-  }
-  /// <summary> 
-  /// Send message to client using socket connection. 
-  /// </summary> 
-  private void SendMessage()
-  {
-    if (connectedTcpClient == null || connectedTcpClient.Available != 0) return;
-    // print();
-    
-    try
-    {
-      // Get a stream object for writing.
-      NetworkStream stream = connectedTcpClient.GetStream();
-      if (stream.CanWrite)
+      catch (SocketException socketException)
       {
-        string serverMessage = "This is a message from your server.";
-        // Convert string message to byte array.
-        byte[] serverMessageAsByteArray = Encoding.ASCII.GetBytes(serverMessage);
-        // Write byte array to socketConnection stream.
-        stream.Write(serverMessageAsByteArray, 0, serverMessageAsByteArray.Length);
-        Debug.Log("Server sent his message - should be received by client");
+        logging.CustomLog(
+          "SocketException " + socketException.ToString(),
+          LogImportance.Crucial, LogStyle.Warning
+        );
       }
     }
-    catch (SocketException socketException)
+
+    /// <summary>
+    /// Send SocketMessage to client using socket connection. 
+    /// </summary>
+    /// <param name="serverSocketMessage">SocketMessage to send to the client</param>
+    private void SocketSend(string serverSocketMessage)
     {
-      Debug.Log("Socket exception: " + socketException);
+      if (connectedTcpClient == null || connectedTcpClient.Available != 0) return;
+      
+      try
+      {
+        // Get a stream object for writing.
+        NetworkStream stream = connectedTcpClient.GetStream();
+        if (stream.CanWrite)
+        {
+          // string serverSocketMessage = "This is a SocketMessage from your server.";
+          // Convert string SocketMessage to byte array.
+          byte[] serverSocketMessageAsByteArray = Encoding.ASCII.GetBytes(serverSocketMessage);
+          // Write byte array to socketConnection stream.
+          stream.Write(serverSocketMessageAsByteArray, 0, serverSocketMessageAsByteArray.Length);
+          logging.CustomLog(
+            "Server sent his SocketMessage - should be received by client",
+            LogImportance.Normal, LogStyle.Log);
+        }
+      }
+      catch (SocketException socketException)
+      {
+        logging.CustomLog(
+          "SocketException " + socketException.ToString(),
+          LogImportance.Crucial, LogStyle.Warning
+        );
+      }
     }
-  } 
+
+    private void ExecuteSocketTask(SocketMessage socketMessage, out string methodOutput)
+    {
+      methodOutput = "";
+      // bunch of returns to avoid errors
+      if (socketMessage.taskType > maxTaskType)
+      {
+        logging.CustomLog(
+          $"taskType recieved from client: {socketMessage.taskType} exceeds the number of task types: {maxTaskType}",
+          LogImportance.Critical, LogStyle.Error);
+        return;
+      }
+
+      if (socketMessage.taskType > 0)
+      {
+        if (socketMessage.argument == null)
+        {
+          logging.CustomLog(
+            $"argument recieved from client is null but is required for taskType: {socketMessage.taskType}",
+            LogImportance.Critical, LogStyle.Error);
+          return;
+        }
+      }
+
+      switch(socketMessage.taskType)
+      {
+        case 0:
+          if (CheckTaskViability(socketMessage.task, socketActions.Length))
+            socketActions[socketMessage.task].Invoke();
+          return;
+
+        case 1:
+          if (CheckTaskViability(socketMessage.task, socketInputActions.Length))
+            socketInputActions[socketMessage.task].Invoke(socketMessage.argument);
+          return;
+
+        case 2:
+          if (CheckTaskViability(socketMessage.task, socketFuncs.Length))
+            methodOutput = socketFuncs[socketMessage.task].Invoke(socketMessage.argument);
+          return;
+      }
+    }
+
+    private bool CheckTaskViability(in int task, in int maxTask)
+    {
+      if (task > maxTask)
+      {
+        logging.CustomLog(
+        $"task recieved from client: {task} exceeds the number of tasks: {maxTask}",
+        LogImportance.Critical, LogStyle.Error);
+        return false;
+      }
+      return true;
+    }
+  }
 }
